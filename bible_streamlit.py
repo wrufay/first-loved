afd9dd2b307d51274d59cff60e9abe13a5e4e96d
fay wu <wru.fay@gmail.com>
2025-12-05 17:27:42 -0500

    style changes

--- bible_streamlit.py
+++ bible_streamlit.py
@@ -2,25 +2,24 @@ import streamlit as st
 import requests
 
 
-st.set_page_config(page_title="Bible Search", page_icon="☻", layout="centered")
+st.set_page_config(page_title="Fay's Bible", page_icon="☻", layout="centered")
 
 st.title("Search a Bible Verse (KJV)")
 
 with st.sidebar:
-    st.header("Search Instructions ☻")
+    st.header("Search Instructions")
     st.markdown("""
-    
-    - `John 3` for an entire chapter
-    - `John 3:16` for a single verse
-    - `John 3:16-20` for a range of verses
-    - `John 3:16-4:10` for multiple chapters
+    - Search an entire chapter :red['Philippians 4']
+    - Search a single verse :red['Jeremiah 29:11']
+    - Search for a range of verses :red['Matthew 6:25-34']
+    - Search for multiple chapters :red['Genesis 1-2 or John 3:16-4:10']
     """)
 
 col1, col2, col3 = st.columns([1, 0.5, 0.5])
 
 with col1:
     book = st.text_input("Book Name", placeholder="Genesis")
-    
+
 with col2:
     verse = st.text_input("Chapter + Verse", placeholder="1:1")
 
@@ -39,28 +38,23 @@ def get_verse(book, verse):
         elif response.status_code == 200: # if successful
             bible_content = response.json()
             st.markdown("---")
-            st.badge(f"{bible_content['reference']}:")
+            st.badge(f"{bible_content['reference']}", color="blue")
             verses = bible_content["verses"]
             
             for v in verses:
-                # thsi makes sections which is not good i dont want
                 with st.container():
-                    # st.markdown(v{['verse']})
-                    # what is difference between markdown and write
-                    st.write(f':red[{v['verse']}] {v['text']}')
-                    # st.markdown("") 
+                    st.write(f'`{v['verse']}` {v['text']}')
         
         else:
+            # catch errors
             st.warning(f"Unexpected error. (Status code: {response.status_code})")
-            # wha does this do?
-            # like catching the errors
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
 
-# searching stuff
+
 if search_button:
     if book and verse:
-        with st.spinner("Searching..."):
+        with st.spinner("..."):
             get_verse(book, verse)
     elif book and not verse:
         st.warning("Please enter a chapter and verse.")
@@ -69,6 +63,7 @@ if search_button:
 
 
 
+# footer
 st.markdown("""
 <div style='text-align: center; color: gray;'>
     <small>Made with ❤️ by Fay</small>
