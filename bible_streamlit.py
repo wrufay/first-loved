33b4e26d107884e62493be3bb41c848f23bcb26a
fay wu <wru.fay@gmail.com>
2025-12-06 23:35:35 -0500

    typo fix

--- bible_streamlit.py
+++ bible_streamlit.py
@@ -114,7 +114,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("ask a question...git"):
+if prompt := st.chat_input("ask a question..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
